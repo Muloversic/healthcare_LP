88a55b71fcbc830e549c84b7634823501fa24cb8
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-06-24 13:30:38 +0300

    added script for second carousel

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import * as bootstrap from 'bootstrap';
 import './styles/reset.css';
 import './styles/main.scss';
 
-const multipleCardCarousel = document.querySelector('#carouselService');
+const multipleCardCarouselService = document.querySelector('#carouselService');
+const multipleCardCarouselClients = document.querySelector('#carouselClients');
 if (window.matchMedia('(min-width: 768px)').matches) {
-  const carousel = new bootstrap.Carousel(multipleCardCarousel, {
+  // start carousel one
+  const carouselService = new bootstrap.Carousel(multipleCardCarouselService, {
     interval: false,
   });
 
   const carouselIndicators = document.querySelector('#carouselService .carousel-indicators');
   carouselIndicators.addEventListener('click', (e) => {
-    const carouselItems = carousel._items;
+    const carouselItems = carouselService._items;
     const targetButton = e.target;
     carouselItems.forEach((card, index) => {
       if ([...targetButton.classList].includes('active') && [...card.classList].includes('active')) {
@@ -23,6 +25,51 @@ if (window.matchMedia('(min-width: 768px)').matches) {
       }
     });
   });
+  //   end carousel one
+
+  // 	start carousel two
+  const carouselClients = new bootstrap.Carousel(multipleCardCarouselClients, {
+    interval: false,
+  });
+  const carouselWidth = document.querySelector('#carouselClients .carousel-inner').scrollWidth;
+  const cardWidth = document.querySelector('#carouselClients .carousel-item').scrollWidth;
+  let scrollPosition = 0;
+
+  document.querySelector('#carouselClients .carousel-control-next').addEventListener('click', function () {
+    const lastCarouselItem = carouselClients._items[carouselClients._items.length - 1];
+    const firstCarouselItem = carouselClients._items[0];
+
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      document
+        .querySelector('#carouselClients .carousel-inner')
+        .scroll({ left: scrollPosition, top: 0, behavior: 'smooth' });
+    }
+
+    if ([...firstCarouselItem.classList].includes('active') && ![...lastCarouselItem.classList].includes('active')) {
+      scrollPosition = 0;
+      document.querySelector('#carouselClients .carousel-inner').scroll({ left: 0, top: 0, behavior: 'smooth' });
+    }
+  });
+
+  document.querySelector('#carouselClients .carousel-control-prev').addEventListener('click', function () {
+    const lastCarouselItem = carouselClients._items[carouselClients._items.length - 1];
+    const firstCarouselItem = carouselClients._items[0];
+
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      document
+        .querySelector('#carouselClients .carousel-inner')
+        .scroll({ left: scrollPosition, top: 0, behavior: 'smooth' });
+    }
+
+    if (![...firstCarouselItem.classList].includes('active') && [...lastCarouselItem.classList].includes('active')) {
+      scrollPosition = carouselWidth - cardWidth;
+      document
+        .querySelector('#carouselClients .carousel-inner')
+        .scroll({ left: scrollPosition, top: 0, behavior: 'smooth' });
+    }
+  });
 } else {
-  multipleCardCarousel.classList.add('slide');
+  multipleCardCarouselService.classList.add('slide');
 }
